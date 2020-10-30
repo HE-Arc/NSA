4b1b710a7ce8ff5f8d89649e34a3fec188612789
cedric.campos <cedric.camposcarvalho@he-arc.ch>
2020-10-30 18:19:03 +0100

    Test auth and message (subscription)

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -9,41 +9,58 @@ use Illuminate\Support\Facades\Validator;
 
 use App\Http\Requests\CreateSubscription;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class SubscriptionController extends Controller
 {
     public function subscribe(User $user, Association $association)
     {
-        $rule = [
-            'association_id' => [
-                'required',
-                Rule::unique('subscriptions')->where(function ($query) use($user,$association) {
-                    return $query->where('association_id', $association->id)
-                    ->where('user_id', $user->id);
-                }),
-            ],
-        ];
+        if(Auth::id() == $user->id)
+        {
+            $rule = [
+                'association_id' => [
+                    'required',
+                    Rule::unique('subscriptions')->where(function ($query) use($user,$association) {
+                        return $query->where('association_id', $association->id)
+                        ->where('user_id', $user->id);
+                    }),
+                ],
+                'user_id' => [
+                    'required'
+                ]
+            ];
 
-        $validator = Validator::make([
-            'association_id' => $association->id,
-            'user_id' => $user->id
-        ],$rule);
+            $messages = [
+                'association_id.unique' => 'You are already subscribed.'
+            ];
+
+            $validator = Validator::make([
+                'association_id' => $association->id,
+                'user_id' => $user->id
+            ],$rule,$messages);
 
 
-        if ($validator->fails()) {
-            return redirect()->back()
-                        ->withErrors($validator)
-                        ->withInput();
+
+            if ($validator->fails()) {
+                return redirect()->back()
+                            ->withErrors($validator)
+                            ->withInput();
+            }
+            else
+            {
+                $subscription = new Subscription;
+                $subscription->association_id = $association->id;
+                $subscription->user_id = $user->id;
+
+                $subscription->save();
+
+                return redirect()->back()->with('success', 'You subscribed to '.$association->name);
+            }
         }
         else
         {
-            $subscription = new Subscription;
-            $subscription->association_id = $association->id;
-            $subscription->user_id = $user->id;
-
-            $subscription->save();
-
-            return redirect()->back()->with('success', 'You subscribed to '.$association->name);
+            return redirect()->back()
+                            ->withErrors('Subscription failed');
         }
     }
 }
