b33fad3f8a14ba02bcdde4bcd5ec05c017fc2c2a
Khalil Mohamed <mohamed.khalil@he-arc.ch>
2020-12-04 11:13:36 +0100

    changed database configuration for the deployement

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver'         => 'mysql',
             'url'            => env('DATABASE_URL'),
-            'host'           => env('DB_HOST', '127.0.0.1'),
-            'port'           => env('DB_PORT', '3306'),
-            'database'       => env('DB_DATABASE', 'forge'),
-            'username'       => env('DB_USERNAME', 'forge'),
-            'password'       => env('DB_PASSWORD', ''),
+            'host'           => env('MYSQL_HOST', env('DB_HOST', 'localhost')),
+            'port'           => env('MYSQL_PORT', env('DB_PORT', '3306')),
+            'database'       => env('GROUPNAME', env('DB_DATABASE', 'forge')),
+            'username'       => env('GROUPNAME', env('DB_USERNAME', 'forge')),
+            'password'       => env('PASSWORD', env('DB_PASSWORD', '')),
             'unix_socket'    => env('DB_SOCKET', ''),
             'charset'        => 'utf8mb4',
             'collation'      => 'utf8mb4_unicode_ci',
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix'  => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix'  => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
