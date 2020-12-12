d6541f2451ed28b403df925f8fa3f23a14dc8e1c
Myrmillion <Myrmillion@users.noreply.github.com>
2020-12-12 15:44:32 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[App\Http\Controllers\ActivityController::class,'index'])->name('index');
+Route::get('/', [App\Http\Controllers\ActivityController::class, 'index'])->name('index');
 
 Auth::routes();
 
